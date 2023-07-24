97cb54912ae43fbec88995014c25a35b4f169fe6
qoboq <volkasp@gmail.com>
2023-07-24 14:05:38 +0300

    qwe

---
+++ pages/api/translate.tsx
@@ -0,0 +1,19 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  // Check method
+  if (req.method !== 'POST') {
+    res.setHeader('Allow', ['POST'])
+    res.status(405).end(`Method ${req.method} Not Allowed`)
+    return
+  }
+
+  // Extract text from request body
+  const { text } = req.body;
+
+  // You should replace this with the actual call to the OpenAI API
+  const translation = `Translation of ${text}`;
+
+  // Send translation as the response
+  res.status(200).json({ translation })
+}
